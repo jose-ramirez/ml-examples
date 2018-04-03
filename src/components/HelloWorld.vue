ad554b8c3e87a53f19f07596feb2b7080be87591
Jose Ramirez <a30673083@gmail.com>
2018-04-03 08:19:05 -0300

    Old Faithful dataset locked and loaded.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,94 +1,41 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-  </div>
+    <div id="chart_div" style="width: 900px; height: 500px;"></div>
 </template>
 
 <script>
+import tableData from '../assets/data.json'
+
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: "Welcome to Your Vue.js App"
+    };
+  },
+
+  mounted() {
+    let google = window["google"]
+    google.charts.load("current", { packages: ["corechart"] })
+    google.charts.setOnLoadCallback(drawChart)
+
+    function drawChart() {
+      let d = tableData.map(r => [r.x, r.y])
+      d.unshift(["Eruptions (min)", "Waiting time (min)"])
+      var data = google.visualization.arrayToDataTable(d)
+
+      var options = {
+        title: "Old faithful dataset",
+        hAxis: { title: "Waiting", minValue: 0, maxValue: 5 },
+        vAxis: { title: "Eruptions", minValue: 40, maxValue: 100 },
+        legend: "none"
+      }
+
+      var chart = new google.visualization.ScatterChart(
+        document.getElementById("chart_div")
+      )
+
+      chart.draw(data, options);
     }
   }
 }
@@ -96,7 +43,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
